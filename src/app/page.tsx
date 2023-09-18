50093a01752f3761fa13160840290370e873babb
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-18 18:33:07 +0100

    feat: Hero section added to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Hero } from "@/components/Templates";
 import styles from "./page.module.css";
 
 export default function Home() {
-	return <main className={styles.main}></main>;
+	return (
+		<main className={styles.main}>
+			<Hero />
+		</main>
+	);
 }
