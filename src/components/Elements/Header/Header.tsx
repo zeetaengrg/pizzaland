48a6578a5a7e1c54de70cc2e6d3a88e121e5ca6e
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-18 18:42:26 +0100

    feat: new components Header and About created

---
+++ src/components/Elements/Header/Header.tsx
@@ -0,0 +1,19 @@
+import styles from "./Header.module.scss";
+
+type HeaderType = {
+	title: string;
+	subtitle: string;
+	description: string;
+};
+
+const Header = ({ title, subtitle, description }: HeaderType) => {
+	return (
+		<article className={styles.container}>
+			<h1 className={styles.title}>{title}</h1>
+			<h2 className={styles.subtitle}>{subtitle}</h2>
+			<p className={styles.desc}>{description}</p>
+		</article>
+	);
+};
+
+export default Header;
---
+++ src/components/Templates/About/About.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import { about } from "@/data/about-data";
+import { Header } from "@/components/Elements";
+import styles from "./About.module.scss";
+
+const About = () => {
+	return (
+		<section className={styles.container}>
+			<Header
+				title="About Us"
+				subtitle="Who We Are"
+				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
+			/>
+			<section className={styles.about}>
+				{about.map((item) => (
+					<article key={item.id} className={styles.card}>
+						<figure className={styles.img}>
+							<Image src={item.img} alt={item.title} height={100} width={100} />
+						</figure>
+						<figcaption className={styles.text}>
+							<h1>{item.title}</h1>
+							<p>{item.description}</p>
+						</figcaption>
+					</article>
+				))}
+			</section>
+		</section>
+	);
+};
+
+export default About;
