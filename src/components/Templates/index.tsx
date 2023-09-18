428e82ad11928538afb5a7cad7f971984dc642b5
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-18 19:58:31 +0100

    feat: new components for respective section added

--- src/components/Templates/index.tsx
+++ src/components/Templates/index.tsx
@@ -1,8 +1,8 @@
+export { default as Locations } from "./Locations/Locations";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Notification } from "./Notification/Notification";
 export { default as Footer } from "./Footer/Footer";
 export { default as Hero } from "./Hero/Hero";
-export { default as Locations } from "./Locations/Locations";
 export { default as Newsletter } from "./Newsletter/Newsletter";
 export { default as Testimonials } from "./Testimonials/Testimonials";
 export { default as About } from "./About/About";
