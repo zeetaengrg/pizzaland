6ed33be096f69136f33c62da1f8fa851c86b2337
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-18 19:57:45 +0100

    feat: newsletter component markup added

--- src/components/Templates/Newsletter/Newsletter.tsx
+++ src/components/Templates/Newsletter/Newsletter.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
+
+import { Header } from "@/components/elements";
+import styles from "./Newsletter.module.scss";
 
 const Newsletter = () => {
-	return <div>Newsletter</div>;
+	return (
+		<section className={styles.container}>
+			<Header
+				title="Subscribe to Newsletter"
+				subtitle="Get Discounts and Benefits"
+				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
+			/>
+			<section className={styles.newsletter}>
+				<form>
+					<input type="email" placeholder="Enter Your Email" />
+					<button onClick={(e) => e.preventDefault()}>Send</button>
+				</form>
+			</section>
+		</section>
+	);
 };
-
 export default Newsletter;
