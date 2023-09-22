3954987f1845fb36d70b77638889324bc073c2f7
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-22 16:54:50 +0100

    feat: add header section to the menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Header } from "@/components/elements";
+import styles from "./MenuPage.module.scss";
+import { Sidebar } from "@/components/templates";
 
 const MenuPage = () => {
-	return <div>MenuPage</div>;
+	return (
+		<section className={styles.container}>
+			<Header
+				title="Our Menu"
+				subtitle="Various Regional Taste"
+				description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor vitae purus faucibus ornare."
+			/>
+			<section className={styles.content}>
+				<Sidebar />
+			</section>
+		</section>
+	);
 };
 
 export default MenuPage;
