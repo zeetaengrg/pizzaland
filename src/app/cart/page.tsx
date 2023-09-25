ab01bba86396af081b2cab3251b8feaf6bef4e31
Jiten Gurung <zeetaen.gurung@gmail.com>
2023-09-25 11:15:52 +0100

    feat: cart page markups added

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+"use client";
 
-const CartPage = () => {
-	return <div>CartPage</div>;
+import { useState } from "react";
+// import { useSelector } from "react-redux";
+
+import { CartItems, CustomerInfo, Payment } from "@components/templates";
+import { TabContent, TabItem } from "@components/elements";
+import styles from "./Cart.module.scss";
+
+const Cart = () => {
+	// const products = useSelector((state) => state.cart.products);
+	// const totalAmount = useSelector((state) => state.cart.totalAmount);
+
+	const [activeTab, setActiveTab] = useState(1);
+
+	return (
+		<section className={styles.container}>
+			<div className={styles.tabs}>
+				<ul className={styles.nav}>
+					<TabItem
+						id={1}
+						title="Cart"
+						activeTab={activeTab}
+						setActiveTab={setActiveTab}
+					/>
+					<hr />
+					<TabItem
+						id={2}
+						title="Details"
+						activeTab={activeTab}
+						setActiveTab={setActiveTab}
+					/>
+					<hr />
+					<TabItem
+						id={3}
+						title="Payment"
+						activeTab={activeTab}
+						setActiveTab={setActiveTab}
+					/>
+				</ul>
+				<section className={styles.tab__content}>
+					<TabContent id="1" activeTab={activeTab}>
+						<CartItems
+							products={products}
+							totalAmount={totalAmount}
+							setActiveTab={setActiveTab}
+						/>
+					</TabContent>
+					<TabContent id="2" activeTab={activeTab}>
+						<CustomerInfo setActiveTab={setActiveTab} />
+					</TabContent>
+					<TabContent id="3" activeTab={activeTab}>
+						<Payment
+							products={products}
+							totalAmount={totalAmount}
+							setActiveTab={setActiveTab}
+						/>
+					</TabContent>
+				</section>
+			</div>
+		</section>
+	);
 };
-
-export default CartPage;
+export default Cart;
